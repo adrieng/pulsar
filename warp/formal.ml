6326b3a4b9a2d3cb112e05949ec26337cbd8b058
Adrien Guatto <adrien@guatto.org>
2017-07-21 21:56:40 -0400

    Add copyright information in all files

--- warp/formal.ml
+++ warp/formal.ml
@@ -1,3 +1,16 @@
+(* This file is part of Pulsar, a temporal functional language.
+ * Copyright (C) 2017 Adrien Guatto
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the LICENSE file in the top-level directory.
+ *)
+
 type t =
   | Warp of Periodic.t
   | On of t * t
