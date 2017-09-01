543699b0a2af9897191652a0ebc2dff92b5dadbc
Adrien Guatto <adrien@guatto.org>
2017-09-01 16:22:31 +0200

    Return more precise diagnosis for implicit coercions

--- pulsar-lib/diagnosis.ml
+++ pulsar-lib/diagnosis.ml
@@ -14,38 +14,40 @@
 open Typed_tree.T
 
 module Sub = Source_tree_utils.Sub(Typed_tree.T)
+module Misc = Source_tree_utils.Misc(Typed_tree.T)
 
-let rec diag_buffers_coercion c =
+let rec diag_buffers_coercion ctx_loc c =
   match c.c_desc with
   | CInvertible _ ->
      ()
   | CSeq (c1, c2) | CArr (c1, c2) | CProd (c1, c2) ->
-     diag_buffers_coercion c1;
-     diag_buffers_coercion c2
+     diag_buffers_coercion ctx_loc c1;
+     diag_buffers_coercion ctx_loc c2
   | CWarped (_, c) ->
-     diag_buffers_coercion c
+     diag_buffers_coercion ctx_loc c
   | CDelay (p, q) ->
      let size = Warp.Formal.size q p in
      let body fmt () =
        Format.fprintf fmt "delay of size %a"
                       Warp.Enat.print size
      in
-     Compiler.Diagnostic.info ~loc:c.c_loc ~body ()
+     let loc = if Loc.nowhere = c.c_loc then ctx_loc else c.c_loc in
+     Compiler.Diagnostic.info ~loc ~body ()
 
-
-let rec diag_buffers thing =
+let rec diag_buffers ctx_loc thing =
   match thing with
   | `Coe coe ->
-     diag_buffers_coercion coe
+     diag_buffers_coercion ctx_loc coe
   | _ ->
-     List.iter diag_buffers @@ Sub.sub thing
+     let ctx_loc = Misc.loc thing in
+     List.iter (diag_buffers ctx_loc) @@ Sub.sub thing
 
 let pass =
   let diagnosis_file file =
     if !Options.display_types
     then Format.printf "%a@." Typed_tree.print_interface file.f_annot;
     if Options.(!diag = Buffer || !diag = Everything)
-    then diag_buffers (`File file);
+    then diag_buffers file.f_loc (`File file);
     file
   in
   Compiler.Pass.atomic
